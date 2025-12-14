ebfb001fbe3ebf3bccb3850c33152020ef9b558e
rashellei <rashelleismail15@gmail.com>
2025-12-14 23:48:12 +0200

    Update connect.php

--- login and signup/connect.php
+++ login and signup/connect.php
@@ -1,5 +1,8 @@
+//this is added to config bcs its used bel database...
+//so it should be reoved from here 
 <?php
 $conn = new mysqli('localhost', 'root', '',"plume_db");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
